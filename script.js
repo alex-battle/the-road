d174b0db770e43274adcbcc2969c0f19325cc18c
Alex Battle <58659536+alex-battle@users.noreply.github.com>
2021-03-11 18:41:20 -0500

    nav animation fully functional

--- script.js
+++ script.js
@@ -13,3 +13,13 @@ let i = 0
 Array.from(document.querySelectorAll('.nav-link')).forEach((item)=>{
     item.style.cssText = `background-color: ${colors[i++]}`
 })
+
+// open close nav
+const container = document.querySelector('.container')
+document.querySelector('.open-navbar-icon').addEventListener('click', () => {
+    container.classList.add('change');
+    
+})
+document.querySelector('.close-navbar-icon').addEventListener('click', () => {
+    container.classList.remove('change');
+})
